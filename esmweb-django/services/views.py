c1be60cfd66486927dcd6a68bf3d562491868829
Ademord <druidelf3@gmail.com>
2018-04-11 10:48:37 +0200

    register service ui

--- esmweb-django/services/views.py
+++ esmweb-django/services/views.py
@@ -20,11 +20,31 @@ def service_catalog(request, service_id=None):
         # create a form instance and populate it with data from the request:
         form = ServiceForm(request.POST)
         # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/')
+
+        return HttpResponseRedirect('/')
+
+        import requests
+        url = "http://localhost:8080/v2/et/catalog"
+        payload = "{\n\t\t\"description\": \"dumdum test service\",\n\t\t\"id\": \"b1620b13-7d11-4abc-a762-f34a108ea49c\",\n\t\t\"name\": \"DDS\",\n\t\t\"short_name\": \"DDS\",\n\t\t\"bindable\": true,\n\t\t\"plan_updateable\": false,\n\t\t\"plans\": [\n\t\t\t{\n\t\t\t\t\"bindable\": false,\n\t\t\t\t\"description\": \"plan for dds\",\n\t\t\t\t\"free\": true,\n\t\t\t\t\"id\": \"36ed4b3e-c132-4746-af71-26dee76e59cb\",\n\t\t\t\t\"metadata\": {\n\t\t\t\t\t\"bullets\": \"basic plan\",\n\t\t\t\t\t\"costs\": {\n\t\t\t\t\t\t\"components\": {\n\t\t\t\t\t\t\t\n\t\t\t\t\t\t},\n\t\t\t\t\t\t\"description\": \"On Demand 5 per deployment, 50 per core, 10 per GB ram and 1 per GB disk\",\n\t\t\t\t\t\t\"fix_cost\": {\n\t\t\t\t\t\t\t\"deployment\": 5\n\t\t\t\t\t\t},\n\t\t\t\t\t\t\"name\": \"On Demand 5 + Charges\",\n\t\t\t\t\t\t\"type\": \"ONDEMAND\",\n\t\t\t\t\t\t\"var_rate\": {\n\t\t\t\t\t\t\t\"cpus\": 50,\n\t\t\t\t\t\t\t\"disk\": 1,\n\t\t\t\t\t\t\t\"memory\": 10\n\t\t\t\t\t\t}\n\t\t\t\t\t}\n\t\t\t\t},\n\t\t\t\t\"name\": \"dds_plan\"\n\t\t\t}\n\t\t]\n\t}\n"
+        headers = {
+            'Accept': "application/json",
+            'Content-Type': "application/json",
+            'X-Broker-Api-Version': "2.12",
+            'Cache-Control': "no-cache",
+            'Postman-Token': "de595b7f-9c04-fc4c-7f36-1ad80e7968b1"
+        }
+
+        response = requests.request("PUT", url, data=payload, headers=headers)
+
+        print(response.text)
+
+        # if form.is_valid():
+        #     # process the data in form.cleaned_data as required
+        #     # ...
+        #     # redirect to a new URL:
+        #     return HttpResponseRedirect('/')
+
+        return HttpResponseRedirect('/')
 
     # if a GET (or any other method) we'll create a blank form
     else:
--- esmweb-django/static/js/app.js
+++ esmweb-django/static/js/app.js
@@ -8,12 +8,8 @@ $('#launchPlan').on('click', function() {
 $('#addService').on('click', function() {
     // $("#modal-background").toggleClass("active", 1000);
     $('#createServiceModal').modal({backdrop: 'static', keyboard: false});
-    //init wizard
-    demo.initMaterialWizard();
-    component = $('.btn-next');
-    if (component && component.hasClass('btn')) {
-        component.removeClass('disabled');
-    }
+
+
     $("div[role=tooltip]").remove();
 
 });
@@ -21,5 +17,15 @@ $('#addService').on('click', function() {
 $(".dropdown-menu a").click(function(){
     $(this).parents(".dropdown").find('.btn').html($(this).text() );
     $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+
 });
 
+$(document).on('change', '.selectpicker', function() {
+  var target = $(this).data('target');
+  var show = $("option:selected", this).data('show');
+  $(target).children().addClass('hide');
+  $(show).removeClass('hide');
+});
+$(document).ready(function(){
+	$('.div-toggle').trigger('change');
+});
